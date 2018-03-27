a5990d644a44ec4dd0cd60562789c115c002da4d
Abby Zhang <abbyzhang@abbys-mbp.home>
2018-03-26 16:56:31 -1000

    lotto

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 
+var randomly = Math.floor(Math.random() * 99 + 1);
+random.addEventListener('click', randomNum);
+var myVar = setInterval(function(){randomNum();}, 3000);
+function randomNum(){
+//create random num 1 to 99
+	var randomly = Math.floor(Math.random() * 99 + 1);
+	var displayElem = document.getElementById('display');
+	displayElem.innerHTML = randomly;
+	//var winnerNumElem = document.getElementById('winnerNum');
+	//document.body.appendChild(randomly);
+}
+
